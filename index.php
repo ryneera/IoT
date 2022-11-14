61bf2f31a3e50ea83733c91267e9479ef00aafc4
ryneera <laura.pituk25@gmail.com>
2022-11-14 19:17:16 +0100

    changed code

--- index.php
+++ index.php
@@ -2,11 +2,24 @@
 <html>
 <body>
 
-<h1>Hali :)</h1>
+Name: <input type="text" name="name" value="<?php echo $name;?>">
 
-<?php
-echo "További fejleményekért, kérjük, kövesse a weboldalunkat! :)))))";
-?>
+E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+
+Website: <input type="text" name="website" value="<?php echo $website;?>">
+
+Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+
+Gender:
+<input type="radio" name="gender"
+<?php if (isset($gender) && $gender=="female") echo "checked";?>
+value="female">Female
+<input type="radio" name="gender"
+<?php if (isset($gender) && $gender=="male") echo "checked";?>
+value="male">Male
+<input type="radio" name="gender"
+<?php if (isset($gender) && $gender=="other") echo "checked";?>
+value="other">Other
 
 </body>
 </html>
