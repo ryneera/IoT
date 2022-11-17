9990bbc99cfe55d6f71477e60e14caa297d0a25f
ryneera <laura.pituk25@gmail.com>
2022-11-17 15:32:12 +0100

    change

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 <html>
 <body>
 
-Name: <input type="text" name="name" value="<?php echo $name;?>">
+Name: <input type="text" name="name" value="<?php echo $name;?>"\n>
 
-E-mail: <input type="text" name="email" value="<?php echo $email;?>">
+E-mail: <input type="text" name="email" value="<?php echo $email;?>"\n>
 
-Website: <input type="text" name="website" value="<?php echo $website;?>">
+Website: <input type="text" name="website" value="<?php echo $website;?>"\n>
 
-Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
+Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?>\n</textarea> 
 
 Gender:
 <input type="radio" name="gender"
@@ -19,7 +19,7 @@ value="female">Female
 value="male">Male
 <input type="radio" name="gender"
 <?php if (isset($gender) && $gender=="other") echo "checked";?>
-value="other">Other
+value="other">Other 
 
 </body>
 </html>
