15b1f6c62bc5f8d608235abe87617668ddddcc2b
ryneera <laura.pituk25@gmail.com>
2022-11-19 16:07:54 +0100

    d

--- index.php
+++ index.php
@@ -1,22 +1,8 @@
 <?php
         
-        $sn1 = $_GET["a"];
-        $sn2 = $_GET["b"];
-        
-        $file_name = "sensors.txt"; 
-        $file1 = fopen($file_name,"w") or die("Unable to open file!");
-        $text1 = "a=" . $sn1 . " b=" . $sn2;
-        
-        fwrite($file1, $text1);
-        fclose($file1);
-        
-        $file2 = fopen("actuator.txt","w") or die("Unable to open file!");
-        $text2 = "Value from actuator. Save this value to actuator.txt :)";
-        fwrite($file2, $text2);
-        fclose($file2);
-        
-        $file3 = fopen("actuator.txt","r") or die ("Subor neexistuje");
-        $text3 = fread($file3,filesize("actuator.txt"));
-        echo $text3;
-        fclose($file3);
+        $file = fopen("anyu.txt", "w");
+        fwrite($file, "szia :)");
+        fclose($file);
+
+        echo "csu";
     ?>
