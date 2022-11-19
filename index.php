f90ba96426b7571751ff46d94c2be3a176597721
ryneera <laura.pituk25@gmail.com>
2022-11-19 17:09:39 +0100

    d

--- index.php
+++ index.php
@@ -12,12 +12,20 @@
                 <input type="email" name="email"  autocomplete="off" required>
             </div>
             <div class="input-box">
+                <label for="">Password: </label>
+                <input type="password" name="password"  autocomplete="off" required>
+            </div>
+            <div class="input-box">
                 <label for="">Age: </label>
                 <input type="number" name="age"  autocomplete="off" required>
             </div>
             <div class="input-box">
                 <label for="">Phone: </label>
                 <input type="tel" name="phone"  autocomplete="off" required>
+            </div>
+            <div class="input-box">
+                <label for="">Gender: </label>
+                <input type="text" name="gender"  autocomplete="off" required>
             </div>
         <input type="submit" value="Save">  
     </form>
