4a25977cbaea818bbea648a95f91c81e09224555
ryneera <laura.pituk25@gmail.com>
2022-11-19 16:46:51 +0100

    yopp

--- config.php
+++ config.php
@@ -6,8 +6,10 @@
     fwrite($file, $name ."\n");
     fwrite($file,"Email :");
     fwrite($file, $email ."\n");
-    fwrite($file,"Password :");
+    fwrite($file,"Age :");
     fwrite($file, $age ."\n");
+    fwrite($file,"Phone :");
+    fwrite($file, $phone ."\n");
     fclose($file);
     header("location: index.php");
  ?>
