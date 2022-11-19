5a2d0bb6642213ed787abac6a72318490244dfae
ryneera <laura.pituk25@gmail.com>
2022-11-19 17:13:11 +0100

    ad

--- config.php
+++ config.php
@@ -2,14 +2,18 @@
     extract($_REQUEST);
     $file=fopen("form-save.txt","w");
 
-    fwrite($file,"name :");
+    fwrite($file,"name: ");
     fwrite($file, $name ."\n");
-    fwrite($file,"Email :");
+    fwrite($file,"Email: ");
     fwrite($file, $email ."\n");
-    fwrite($file,"Age :");
+    fwrite($file,"Password: ");
+    fwrite($file, $password ."\n");
+    fwrite($file,"Age: ");
     fwrite($file, $age ."\n");
-    fwrite($file,"Phone :");
+    fwrite($file,"Phone: ");
     fwrite($file, $phone ."\n");
+    fwrite($file,"Gender: ");
+    fwrite($file, $gender ."\n");
     fclose($file);
     header("location: index.php");
  ?>
