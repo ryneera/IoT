a7ae629e1f432f7d2ec64443c67324d6042db550
ryneera <laura.pituk25@gmail.com>
2022-11-19 15:50:22 +0100

    v

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
         $sn1 = $_GET["a"];
         $sn2 = $_GET["b"];
         
-        $file1 = fopen("sensors.txt","w") or die("Unable to open file!");
+        $file_name = "sensors.txt"; 
+        $file1 = fopen($file_name,"w") or die("Unable to open file!");
         $text1 = "a=" . $sn1 . " b=" . $sn2;
         
         fwrite($file1, $text1);
